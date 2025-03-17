25395429a72b6ee08584f109ac7640f856b190af
Bomina KIM <bmnkimx@gmail.com>
2025-03-17 16:48:18 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("동물 이미지 찾아주기 🐈‍⬛")
+
+title = st.text_input("영어로 동물 이름을 입력해주세요")
+if st.button('찾아 보기'):
+    url = 'https://edu.spartacodingclub.kr/random/?'+title
+    st.image(url)
